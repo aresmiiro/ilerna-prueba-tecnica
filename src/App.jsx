940583abfbac4806bc3856f70b3ce0279bcd018c
aresmiro <ares@rashediconsulting.de>
2025-12-14 20:44:26 +0100

    react

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/modules/Header/Header';
 import Footer from './components/modules/Footer/Footer';
 import Home from './components/templates/Home/Home';
 import './styles/globals.scss';
+import React from 'react';
 
 function App() {
   return (
